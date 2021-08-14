d60ec8808ee25f41da16acb7eafb32c756c10ec4
erich <lzujing@gmail.com>
2021-08-14 14:25:05 +0800

    bump deps

--- auth_ticket_test.go
+++ auth_ticket_test.go
@@ -23,10 +23,10 @@ import (
 func TestAuthTicketVerifySignature(t *testing.T) {
 	signKey := "5f0fa825de41a7d3fd000002"
 	ts := "1594800736455"
-	subject := "1"
+	subject := "admin"
 	requestID := "xxx"
-	token := "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MjYzOTc2MDUuNjIyNTY0LCJzdWIiOiIxIn0.4S2w33nYU5rzOjPePP5t4YUnNVmRGJKlUxu8_ioogoU"
-	sig := "d3dee5e6260b6a2ce59bc7a6a1a14d024756770cc7a6b5ec9a7ee4a9c5dc82d7"
+	token := "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjIyMjY3MTIzMDkuODY5ODM5Nywic3ViIjoiYWRtaW4iLCJ1c2VySWQiOjF9.2M1tC3oQrC0Ym7K9qfVVcLS-eyCM4bacVOyuh6Jj1yg"
+	sig := "b7dc13da9157cdaf86e405063476c9a5dc2e677d420a06fd8195c9d3229cceba"
 
 	ticket := &AuthTicket{
 		Timestamp: ts,
--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module nfgo.ga/nfgo/x/nsecurity
 go 1.16
 
 require (
-	github.com/casbin/casbin/v2 v2.31.0
-	github.com/casbin/gorm-adapter/v3 v3.2.12
-	github.com/gomodule/redigo v1.8.4
+	github.com/casbin/casbin/v2 v2.35.2
+	github.com/casbin/gorm-adapter/v3 v3.3.3
+	github.com/gomodule/redigo v1.8.5
 	github.com/pascaldekloe/jwt v1.10.0
 	github.com/stretchr/testify v1.7.0
-	gorm.io/gorm v1.21.10
-	nfgo.ga/nfgo v0.2.5
+	gorm.io/gorm v1.21.13
+	nfgo.ga/nfgo v0.2.6
 )
